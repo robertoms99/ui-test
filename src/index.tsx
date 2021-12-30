e31ab7f4e91f3669ae692877e2635ffdfa3296d5
Roberto Molina <rmolinas1999@gmail.com>
2021-12-30 15:10:26 -0500

    feat: initial cleanup src

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
 
-ReactDOM.render(
+const RootComponent = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const $rootContainer = document.getElementById('root') ?? document.createElement('div')
+const $bodyElement = document.body
+
+$rootContainer.id = 'root'
+
+if (!$bodyElement.contains($rootContainer)) {
+  $bodyElement.appendChild($rootContainer)
+}
+
+render(RootComponent, $rootContainer)
